5714fae457a20ddf9ca3981293bdb83efda922aa
otoniel0003 <l24550281@chihuahua2.tecnm.mx>
2026-08-31 20:04:12 -0600

    Add files via upload

---
+++ EVA_1_SCOPE/src/main/java/com/mycompany/eva_1_scope/EVA_1_SCOPE.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_1_scope;
+
+/**
+ *
+ * @author Salid
+ */
+public class EVA_1_SCOPE {
+
+    public static void main(String[] args) {
+        int x = 100;
+        for (int i = 0; i < 10; i++){
+            System.out.println("i = " + i);
+        }
+        System.out.println("Valor final de i = " + i);
+        System.out.println("Vvalor finald de la x = " + x);
+    }
+    public static void OtraFuncion(){
+        System.out.println("Valor de x " + x);
+    }
+}
