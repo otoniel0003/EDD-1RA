c15426f0fdc2222b46a859bc28af63e34839083a
otoniel0003 <l24550281@chihuahua2.tecnm.mx>
2026-09-03 16:55:57 -0600

    Add files via upload

---
+++ EVA_1_3_STACKOVERFLOW/src/main/java/com/mycompany/eva_1_3_stackoverflow/EVA_1_3_STACKOVERFLOW.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_1_3_stackoverflow;
+
+/**
+ *
+ * @author Salid
+ */
+public class EVA_1_3_STACKOVERFLOW {
+
+    public static void main(String[] args) {
+        A();
+    }
+    public static void A(){
+        B();
+} 
+    public static void B(){
+        int i = 5;
+        double d = 5.5;
+        A();
+    }
+    
+}
