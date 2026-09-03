fdca37b0814a4bcb774ad1aff418dc725f8c3692
otoniel0003 <l24550281@chihuahua2.tecnm.mx>
2026-09-03 16:55:29 -0600

    Add files via upload

---
+++ EVA_1_2_STACK/src/main/java/com/mycompany/eva_1_2_stack/EVA_1_2_STACK.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_1_2_stack;
+
+/**
+ *
+ * @author Salid
+ */
+public class EVA_1_2_STACK {
+
+    public static void main(String[] args) {
+        System.out.println("INICIA main");
+        A();
+        System.out.println("TERMINA main");
+    }
+    public static void A(){
+        System.out.println("INICIA A()");
+        B();
+        System.out.println("TERMINA A()");
+       
+    }
+    public static void B(){
+        System.out.println("INICIA B()");
+        int i = 5;
+        double d = 5.5;
+        System.out.println("INICIA B()");
+
+    }
+}
