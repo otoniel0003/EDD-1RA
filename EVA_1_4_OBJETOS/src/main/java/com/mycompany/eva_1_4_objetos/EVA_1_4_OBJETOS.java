a3fdcd9960b66cf4231a610e7e48d404f1937895
otoniel0003 <l24550281@chihuahua2.tecnm.mx>
2026-09-07 16:53:08 -0600

    Add files via upload

---
+++ EVA_1_4_OBJETOS/src/main/java/com/mycompany/eva_1_4_objetos/EVA_1_4_OBJETOS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_1_4_objetos;
+
+/**
+ *
+ * @author Salid
+ */
+public class EVA_1_4_OBJETOS {
+
+    public static void main(String[] args) {
+        Prueba prueba = new Prueba();
+        System.out.println(prueba);
+    }
+}
+class Prueba{
+    
+}
