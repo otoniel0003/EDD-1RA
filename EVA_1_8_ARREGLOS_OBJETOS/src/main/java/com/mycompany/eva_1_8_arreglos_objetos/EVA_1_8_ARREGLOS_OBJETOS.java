8ac69ad63819b4c1d85d6eac45eedbc0a71a171b
otoniel0003 <l24550281@chihuahua2.tecnm.mx>
2026-09-15 14:51:07 -0600

    Add files via upload

---
+++ EVA_1_8_ARREGLOS_OBJETOS/src/main/java/com/mycompany/eva_1_8_arreglos_objetos/EVA_1_8_ARREGLOS_OBJETOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_1_8_arreglos_objetos;
+
+/**
+ *
+ * @author Salid
+ */
+public class EVA_1_8_ARREGLOS_OBJETOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
